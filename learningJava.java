7b8c60d69b2e34794dcb60acb7fa48b81e5a0f5b
AbdulKalam21 <abdulkalam2185@gmail.com>
2024-06-23 18:32:07 +0530

    Second question of leacture 2

--- learningJava.java
+++ learningJava.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class learningJava{
 
 	public static void main(String[] args) {
-		System.out.println("*");
-		System.out.println("**");
-		System.out.println("***");
-		System.out.println("****");
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int sum = a+b;
+		System.out.println(sum);
 	}
 
 }
