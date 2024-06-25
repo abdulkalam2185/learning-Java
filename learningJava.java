f673c9542004dbb8b3d00e0bfe5d25421e48db1d
AbdulKalam21 <abdulkalam2185@gmail.com>
2024-06-25 16:09:25 +0530

    Seconds question of lecture 4

--- learningJava.java
+++ learningJava.java
@@ -3,10 +3,15 @@ import java.util.*;
 public class learningJava{
 
 	public static void main(String[] args) {
-		
-	for (int i = 0; i < 11; i++) {
-			System.out.println(i);		
-		}		
-	}
 
-}
+		Scanner sc = new Scanner(System.in);
+
+		int number = sc.nextInt();
+		int sum = 0;	
+		for (int i= 1; i <= number; i++) {
+			sum = sum + i;
+			
+		}
+		System.out.println(sum);
+	}
+} 
