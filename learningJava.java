7263aec0f228515750455e8660ac6b46b7dc4b1f
AbdulKalam21 <abdulkalam2185@gmail.com>
2024-06-25 15:26:48 +0530

    Second homework of lecture 3

--- learningJava.java
+++ learningJava.java
@@ -6,32 +6,49 @@ public class learningJava{
 		
 		Scanner sc = new Scanner(System.in);
 
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		String operation = sc.next();
+		int number = sc.nextInt();
 
-		switch(operation){
-		case "Addition":
-			System.out.print(a + b);
+		switch (number) {
+		case 1:
+			System.out.print("January");
 			break;
-		case "Subtraction":
-			System.out.print(a - b);
+		case 2:
+			System.out.print("February");
 			break;
-		case "Multiplication":
-			System.out.print(a * b);
+		case 3:
+			System.out.print("March");
 			break;
-		case "Division":
-			System.out.print(a / b);
+		case 4:
+			System.out.print("April");
 			break;
-		case "Modulo":
-			System.out.print(a % b);
+		case 5:
+			System.out.print("May");
+			break;
+		case 6:
+			System.out.print("June");
+			break;
+		case 7:
+			System.out.print("July");
+
+		case 8:
+			System.out.print("August ");
+			break;
+		case 9:
+			System.out.print("September");
+			break;
+		case 10:
+			System.out.print("October");
+			break;
+		case 11:
+			System.out.print("November");
+			break;
+		case 12:
+			System.out.print("December");
 			break;
 		default:
-			System.out.print("Invalid operation");
+			System.out.print("Invalid number");
 			break;
 		}
-		
-
 	}
 
 }
