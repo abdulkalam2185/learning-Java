024533e3b78a98a80e5b856d958da61300372692
AbdulKalam21 <abdulkalam2185@gmail.com>
2024-06-25 15:12:00 +0530

    2nd homework of lecture 2

--- learningJava.java
+++ learningJava.java
@@ -3,23 +3,13 @@ import java.util.*;
 public class learningJava{
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		int button = sc.nextInt();
 
-		switch(button) {
-		case 1:
-				System.out.println("Hello");
-				break;
-		case 2:
-				System.out.print("Namaste");
-				break;
-		case 3:
-				System.out.print("Bonjour");
-				break;
-		default:
-				System.out.print("Invalid Button");
-	
-		}
+		double radius = sc.nextDouble();
+
+		double area = 3.14 * radius * radius;
+		System.out.print(area);
 		
 
 	}
