012f0783ea0d8f57ac50adbc80b27d6edf0509a7
AbdulKalam21 <abdulkalam2185@gmail.com>
2024-06-25 15:44:59 +0530

    3rd homework of lecture 2

--- learningJava.java
+++ learningJava.java
@@ -7,48 +7,20 @@ public class learningJava{
 		Scanner sc = new Scanner(System.in);
 
 		int number = sc.nextInt();
+						
+				System.out.println(number * 2);
+				System.out.println(number * 3);
+				System.out.println(number * 4);
+				System.out.println(number * 5);
+				System.out.println(number * 6);
+				System.out.println(number * 7);
 
-		switch (number) {
-		case 1:
-			System.out.print("January");
-			break;
-		case 2:
-			System.out.print("February");
-			break;
-		case 3:
-			System.out.print("March");
-			break;
-		case 4:
-			System.out.print("April");
-			break;
-		case 5:
-			System.out.print("May");
-			break;
-		case 6:
-			System.out.print("June");
-			break;
-		case 7:
-			System.out.print("July");
+				System.out.println(number * 8);
 
-		case 8:
-			System.out.print("August ");
-			break;
-		case 9:
-			System.out.print("September");
-			break;
-		case 10:
-			System.out.print("October");
-			break;
-		case 11:
-			System.out.print("November");
-			break;
-		case 12:
-			System.out.print("December");
-			break;
-		default:
-			System.out.print("Invalid number");
-			break;
-		}
+				System.out.println(number * 9);
+
+				System.out.println(number * 10);
+		
 	}
 
 }
