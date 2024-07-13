1a983c37112c54fcdeac8ec6bc2d17fe651666a6
AbdulKalam21 <abdulkalam2185@gmail.com>
2024-07-13 09:50:19 +0530

    Third Homework of lecture 4

--- learningJava.java
+++ learningJava.java
@@ -4,9 +4,23 @@ public class learningJava{
 
 	public static void main(String[] args) {
 
-		for (; ; ) {
-			System.out.println("Apna College");
+		Scanner sc = new Scanner(System.in);
+
+		int number = sc.nextInt();
+
+		if(number == 1){
+			int marks = sc.nextInt();
+			if(marks <= 100){
+
+				if(marks >= 90){
+					System.out.println("This is Good");
+				} else if(marks >= 89 || marks >=60){
+					System.out.println("This is also Good");
+				} else if(marks>=59 ||marks >=0){
+					System.out.println("This is good as well");
+				}
+
+			}
 		}
-		
 	}
 } 
