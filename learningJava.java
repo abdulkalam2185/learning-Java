bbb8e1ff7228efa82bda9cdffbeb7fd3ec4aff17
AbdulKalam21 <abdulkalam2185@gmail.com>
2024-07-13 09:14:47 +0530

    Second Homework of lecture 4

--- learningJava.java
+++ learningJava.java
@@ -4,12 +4,8 @@ public class learningJava{
 
 	public static void main(String[] args) {
 
-		int n = 19;
-		
-		for (int i=0; i<n; i++) {
-			if (i%2 == 0) {
-				System.out.println("n is even " + i);
-			}
+		for (; ; ) {
+			System.out.println("Apna College");
 		}
 		
 	}
