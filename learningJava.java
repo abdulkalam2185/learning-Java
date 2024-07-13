b3f6f3fb7ab4a25cd490469f34085660f046791e
abdulkalam2185 <siddiqueabdulkalam2185@gmail.com>
2024-07-13 11:10:08 +0530

    Third homework of lecture 4

--- learningJava.java
+++ learningJava.java
@@ -6,11 +6,25 @@ public class learningJava{
 
 		Scanner sc = new Scanner(System.in);
 
-		int number = sc.nextInt();
+		int input;
 
-		if(number%2 == 1){
-			System.out.println("Number is prime " + number);
-		}
+		do{
+			int marks = sc.nextInt();
+			if(marks >= 90 && marks <=100){
+				System.out.println("This is good");
+			} else if(marks >= 60 && marks <= 89){
+				System.out.println("This is also good");
+			} else if(marks >= 0 && marks <= 59){
+				System.out.println("This is good as well");
+			} else {
+				System.out.println("Invalid");
+			}
+
+			System.out.println("Want to continue? (yes(1) or no(0)");
+
+		input = sc.nextInt();
+
+		}while(input==1);
 
 	}
 } 
