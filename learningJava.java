63a4443a882b419e93a61a3ff8c105aeba3391ce
abdulkalam2185 <siddiqueabdulkalam2185@gmail.com>
2024-07-13 11:40:23 +0530

    First questin of lecture 7

--- learningJava.java
+++ learningJava.java
@@ -2,29 +2,24 @@ import java.util.*;
 
 public class learningJava{
 
+
+	public static void printFactorial(int n){
+		if(n < 0){
+			System.out.println("Invalid Number");
+			return;
+		}
+		int factorial = 1;
+		for(int i =n; i>=1; i--){
+			factorial = factorial * i;
+		
+		}
+		System.out.println(factorial);
+	}
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-
-		int input;
-
-		do{
-			int marks = sc.nextInt();
-			if(marks >= 90 && marks <=100){
-				System.out.println("This is good");
-			} else if(marks >= 60 && marks <= 89){
-				System.out.println("This is also good");
-			} else if(marks >= 0 && marks <= 59){
-				System.out.println("This is good as well");
-			} else {
-				System.out.println("Invalid");
-			}
-
-			System.out.println("Want to continue? (yes(1) or no(0)");
-
-		input = sc.nextInt();
-
-		}while(input==1);
+		int n = sc.nextInt();
+		printFactorial(n);
+		
 
 	}
 } 
